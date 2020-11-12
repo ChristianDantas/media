540721e200b4f91c2e6df25f2dc77c69cf072f5c
Christian <christian_d_ol9000@hotmail.com>
2020-11-12 16:38:03 -0300

    ok

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite a primeira nota: ");
+            double nota1= double.Parse(Console.ReadLine());
+            Console.WriteLine("digite a segunda nota: ");
+            double nota2= double.Parse(Console.ReadLine());
+
+            double media= (nota1 + nota2) / 2;
+            string situaçao= "";
+
+            if(media >7){
+                situaçao= "aprovado";
+            }
+            else{
+                situaçao= "reprovado";
+            }
+            Console.WriteLine(situaçao);
+        }
+    }
+}
